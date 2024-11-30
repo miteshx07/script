22610d372f87c6e2fd1c0a3572e59c4879cdae82
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-11-30 12:18:34 +0530

    Update sakura.sh

--- sakura.sh
+++ sakura.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# rm -rf .repo/local_manifests/
+rm -rf .repo/local_manifests
 
 # Rom source repo
 repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
@@ -8,32 +8,28 @@ echo "=================="
 echo "Repo init success"
 echo "=================="
 
-# Clone local_manifests repository
-# git clone -b sakura https://github.com/miteshx07/local_manifest.git .repo/local_manifests
-# echo "============================"
-# echo "Local manifest clone success"
-# echo "============================"
+Clone local_manifests repository
+git clone -b sakura https://github.com/miteshx07/local_manifest.git .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Sync the repositories
 /opt/crave/resync.sh
 echo "============================"
 
-# Export
-export BUILD_USERNAME=Mit_Oz
-export BUILD_HOSTNAME=crave
-export TZ=Asia/DELHI
-export SAKURA_MAINTAINER=Mit_Oz
-export SAKURA_BUILD_TYPE=gapps
-export TARGET_BOOT_ANIMATION_RES=720
-echo "======= Export Done ======"
-
 # Set up build environment
 . build/envsetup.sh
 echo "====== Envsetup Done ======="
+
+export SAKURA_MAINTAINER=Mit_Oz
+export SAKURA_BUILD_TYPE=gapps
+export TARGET_BOOT_ANIMATION_RES=720
+echo "======== Export Done ======="
 
 # Lunch
 lunch lineage_lavender-ap2a-userdebug
 echo "============="
 
 # Build rom
-mka bacon
+m bacon
