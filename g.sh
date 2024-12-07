e19454d4f48bf62d17c05d26cdca7f1950d5fe26
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-12-07 15:48:35 +0530

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
+#rm -rf .repo/local_manifests
 
 # Rom source repo
-repo init --depth=1 -u https://github.com/GenesisOS/manifest.git -b verve --git-lfs
+#repo init --depth=1 -u https://github.com/GenesisOS/manifest.git -b verve --git-lfs
 #echo "=================="
 #echo "Repo init success"
 #echo "=================="
 
 # Clone local_manifests repository
-git clone -b gen https://github.com/miteshx07/local_manifest.git .repo/local_manifests
+#git clone -b gen https://github.com/miteshx07/local_manifest.git .repo/local_manifests
 #echo "============================"
 #echo "Local manifest clone success"
 #echo "============================"
 
 # Sync the repositories
-/opt/crave/resync.sh
-echo "============================"
+#/opt/crave/resync.sh
+#echo "============================"
 
 # Export
-export TZ=Asia/Delhi
-echo "======= Export Done ======"
+#export TZ=Asia/Delhi
+#echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
@@ -31,4 +31,4 @@ breakfast lavender
 echo "============="
 
 # Build rom
-#mka genesis
+mka genesis
