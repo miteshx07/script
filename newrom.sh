52a7c3e2d6421f46b41eefd70ad0e09de2f2e8ca
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-12-10 14:59:50 +0530

    Update newrom.sh

--- newrom.sh
+++ newrom.sh
@@ -4,10 +4,10 @@ rm -rf .repo/local_manifests
 
 repo init -u https://github.com/nitrogen-project/android_manifest.git -b 15 --git-lfs
 
-git clone -b gen https://github.com/miteshx07/local_manifest.git .repo/local_manifests
+git clone -b nitro15 https://github.com/miteshx07/local_manifest.git .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
-breakfast lavender
-mka genesis
+lunch nitrogen_lavender-userdebug
+#make -j$(nproc --all)
