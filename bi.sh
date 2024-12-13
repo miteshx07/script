1d43a582cef7ddfe77d575fd07ac6b389bae9544
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-12-13 14:41:06 +0530

    Create bi.sh

---
+++ bi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+
+repo init -u https://github.com/Black-Iron-Project/manifest -b v15 --git-lfs
+
+git clone -b black https://github.com/miteshx07/local_manifests.git .repo/local_manifests
+
+/opt/crave/resync.sh
+
+. build/envsetup.sh
+
+blkilunch lavender user
+
+blki b
