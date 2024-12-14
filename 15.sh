f201d298b2a9085adc55536d1d7f68b723cf44bd
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-12-14 11:40:57 +0530

    Update 15.sh

--- 15.sh
+++ 15.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
+#rm -rf .repo/local_manifests
 
-repo init -u https://github.com/Black-Iron-Project/manifest -b v15 --git-lfs
+#repo init -u https://github.com/Black-Iron-Project/manifest -b v15 --git-lfs
 
-git clone -b black https://github.com/miteshx07/local_manifests.git .repo/local_manifests
+#git clone -b black https://github.com/miteshx07/local_manifests.git .repo/local_manifests
 
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
 . build/envsetup.sh
 
 blkilunch lavender user
 
-#blki b
+blki b
