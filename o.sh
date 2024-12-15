a87ca40cada36ebe4f21405f458e60600f694782
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-12-15 18:24:07 +0530

    Update o.sh

--- o.sh
+++ o.sh
@@ -2,7 +2,7 @@
 
 rm -rf .repo/local_manifests
 
-repo init -u https://github.com/omnirom/android.git -b android-14.0 --git-lfs
+repo init -u https://github.com/mitmac29/android_.git -b android-15.0 --git-lfs
 
 git clone -b o https://github.com/miteshx07/local_manifests.git .repo/local_manifests
 
