3c2f4a1a82c740d5b5b6731579791b6865dd89db
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-12-20 03:43:26 +0530

    Update sakura.sh

--- sakura.sh
+++ sakura.sh
@@ -1,40 +1,7 @@
 #!/bin/bash
 
-#rm -rf .repo/local_manifests
-
-# Rom source repo
-#repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
-#echo "=================="
-#echo "Repo init success"
-#echo "=================="
-
-# Clone local_manifests repository
-#git clone -b sakura https://github.com/miteshx07/local_manifests .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
-
-# Sync the repositories
-#/opt/crave/resync.sh
-#echo "============================"
-
-# Set up build environment
 . build/envsetup.sh
-echo "====== Envsetup Done ======="
 
-export SAKURA_MAINTAINER=MiTEsH
-export SAKURA_BUILD_TYPE=vanilla
-export SAKURA_BUILD_TYPE=VANILLA
-export TARGET_BOOT_ANIMATION_RES=720
-export TARGET_RELEASE=ap2a
-export TZ=Asia/Dhaka
-export TARGET_BUILD_VARIANT=user
-export TARGET_BUILD_TYPE=user
-echo "======== Export Done ======="
+lunch lineage_lavender-ap2a-userdebug
 
-# Lunch
-lunch lineage_lavender-ap2a-user
-echo "============="
-
-# Build rom
-m bacon
+mka bacon
